20d778230b6429d29b6126cc1017d0fbba99a505
Lee de Mora <ledm@pml.ac.uk>
2024-03-20 09:40:23 +0000

    adding music script

---
+++ esm2.py
@@ -0,0 +1,10 @@
+
+from earthsystemmusic2 import climate_music_maker
+
+#climate_music_maker.climate_music_maker('marineheatwaves_clim.yml')
+#climate_music_maker.climate_music_maker('marineheatwaves.yml')
+#climate_music_maker.climate_music_maker('marineheatwaves_faster.yml')
+climate_music_maker.climate_music_maker('marineheatwaves_faster_mhw_onset.yml')
+
+#climate_music_maker.climate_music_maker('marineheatwaves_bass.yml')
+
